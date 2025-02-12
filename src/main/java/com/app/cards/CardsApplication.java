af3a9504eae1202379bb91ad3a114bcbe4cbe44a
Jaya Krishna Mannem <jayamannem07@gmail.com>
2025-02-12 15:40:06 +0530

    The application now sources it's properties files from an external configserver (not completely)

--- src/main/java/com/app/cards/CardsApplication.java
+++ src/main/java/com/app/cards/CardsApplication.java
@@ -2,11 +2,15 @@ package com.app.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.app.cards.dto.CardsContactInfoDto;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = { CardsContactInfoDto.class })
 public class CardsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CardsApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CardsApplication.class, args);
+  }
 }
